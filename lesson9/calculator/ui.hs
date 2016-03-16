3118695cc09a5ea0954917c318bf007b6ae5fcef
Alexander Myshov <simplex.light@gmail.com>
2016-03-17 02:14:38 +0600

    Add indication of error in calculator

--- lesson9/calculator/ui.hs
+++ lesson9/calculator/ui.hs
@@ -1,9 +1,11 @@
 import Actions
 import Parser
 import Parsing
+import Data.List (findIndex, tails, isPrefixOf)
 
 box :: [String]
 box =  ["+---------------+",
+        "|               |",
         "|               |",
         "+---+---+---+---+",
         "| q | c | d | = |",
@@ -17,6 +19,12 @@ box =  ["+---------------+",
         "| 0 | ( | ) | / |",
         "+---+---+---+---+"]
 
+findSubstringIndex :: String -> String -> Int
+findSubstringIndex text pattern
+     = case findIndex (pattern `isPrefixOf`) (tails text) of
+        Just n -> n
+        Nothing -> -1
+
 buttons :: [Char]
 buttons = standard ++ extra
           where
@@ -24,19 +32,27 @@ buttons = standard ++ extra
             extra = "QCD \ESC\BS\DEL\n"
 
 showbox :: IO ()
-showbox = seqn [writeAt (1, y) xs | (y, xs) <- zip [1..13] box]
+showbox = seqn [writeAt (1, y) xs | (y, xs) <- zip [1..14] box]
+
+fitScreen :: String -> String
+fitScreen xs = reverse (take 13 (reverse xs))
 
 display :: String -> IO ()
 display xs = do writeAt (2, 2) "               "
-                writeAt (2, 2) (reverse (take 13 (reverse xs)))
+                writeAt (2, 2) (fitScreen xs)
+                writeAt (2, 3) "               "
+
+displayErr :: String -> IO ()
+displayErr xs = do writeAt (2, 3) "               "
+                   writeAt (2, 3) xs
 
 calc :: String -> IO ()
-calc xs = do display xs
-             c <- getCh
+calc xs = do c <- getCh
              if elem c buttons then
                  process c xs
              else
                  do beep
+                    display xs
                     calc xs
 
 process :: Char -> String -> IO ()
@@ -51,20 +67,36 @@ quit :: IO ()
 quit = goto (1, 14)
 
 delete :: String -> IO ()
-delete "" = calc ""
-delete xs = calc (init xs)
+delete "" = do display ""
+               calc ""
+delete xs = do display (init xs)
+               calc (init xs)
+
+replaceTilda :: String -> String
+replaceTilda xs = take (length xs) (repeat '~')
+
+prepareErr :: String -> String -> String
+prepareErr orig part = (take pos (repeat ' ')) ++ (replaceTilda part)
+                       where
+                            pos = findSubstringIndex orig part
 
 evalExp :: String -> IO ()
 evalExp xs = case parse expr xs of
-            [(n, "")] -> calc (show n)
-            _ -> do beep
-                    calc xs
+            [(n, "")] -> do display (show n)
+                            calc (show n)
+            [(n, part)] -> do displayErr (fitScreen (prepareErr xs part))
+                              beep
+                              calc xs
+                              display xs
 
 clear :: IO ()
-clear = calc ""
+clear = do display ""
+           calc ""
+
 
 press :: Char -> String -> IO ()
-press c xs = calc (xs ++ [c])
+press c xs = do display (xs ++ [c])
+                calc (xs ++ [c])
 
 run :: IO ()
 run = do cls
